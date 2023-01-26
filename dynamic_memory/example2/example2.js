efafae637ba207d377f781849c7cb1c9aa512db2
Jidong Xiao <xiaoj8@rpi.edu>
2023-01-26 01:47:40 -0500

    adding example2, fixing problems in example 1

--- dynamic_memory/example2/example2.js
+++ dynamic_memory/example2/example2.js
@@ -65,12 +65,13 @@ function makeMemory(xstart,ystart,bufferSize,w,h) {
 
 // create stack
 makeMemory(750,100,6,200,100);
-makeText(750+250, 175, "0x1014", "addr0");
-makeText(750+250, 275, "0x1010", "addr1");
-makeText(750+250, 375, "0x100C", "addr2");
-makeText(750+250, 475, "0x1008", "addr3");
-makeText(750+250, 575, "0x1004", "addr4");
-makeText(750+250, 675, "0x1000", "addr5");
+makeText(750+250, 75, "0x1030", "addr0");
+makeText(750+250, 175, "0x1028", "addr1");
+makeText(750+250, 275, "0x1020", "addr2");
+makeText(750+250, 375, "0x1018", "addr3");
+makeText(750+250, 475, "0x1010", "addr4");
+makeText(750+250, 575, "0x1008", "addr5");
+makeText(750+250, 675, "0x1000", "addr6");
 
 function makeHeap(xstart,ystart,bufferSize,w,h) {
     for(let i=0;i<bufferSize;i++){
@@ -95,12 +96,12 @@ function makeHeap(xstart,ystart,bufferSize,w,h) {
 
 // create heap
 makeHeap(1200,100,6,200,100);
-makeText(750+690, 80, "0x0218", "haddr0");
-makeText(750+690, 180, "0x0214", "haddr1");
-makeText(750+690, 280, "0x0210", "haddr2");
-makeText(750+690, 380, "0x020C", "haddr3");
-makeText(750+690, 480, "0x0208", "haddr4");
-makeText(750+690, 580, "0x0204", "haddr5");
+makeText(1480, 80, "0x0228", "haddr0");
+makeText(1480, 180, "0x0220", "haddr1");
+makeText(1480, 280, "0x0218", "haddr2");
+makeText(1480, 380, "0x0210", "haddr3");
+makeText(1480, 480, "0x0208", "haddr4");
+makeText(1480, 580, "0x0200", "haddr5");
 
 // the code rectangle.
 var rect1 = new Konva.Rect({
@@ -110,8 +111,8 @@ var rect1 = new Konva.Rect({
     stroke: '#555',
     strokeWidth: 5,
     fill: '#ddd',
-    width: 400,
-    height: 320,
+    width: 550,
+    height: 300,
     shadowColor: 'black',
     shadowBlur: 10,
     shadowOffsetX: 10,
@@ -127,8 +128,8 @@ var rect2 = new Konva.Rect({
     stroke: '#555',
     strokeWidth: 5,
     fill: '#ddd',
-    width: 400,
-    height: 220,
+    width: 550,
+    height: 200,
     shadowColor: 'black',
     shadowBlur: 10,
     shadowOffsetX: 10,
@@ -225,7 +226,7 @@ var consoleText = new Konva.Text({
 });
 
 var arrow1 = new Konva.Arrow({
-    points: [880, 148, 1200, 395],
+    points: [880, 248, 1200, 595],
     tension: 0.5,
     pointerLength: 10,
     pointerWidth: 10,
@@ -236,7 +237,7 @@ var arrow1 = new Konva.Arrow({
 });
 
 var arrow2 = new Konva.Arrow({
-    points: [880, 248, 1200, 595],
+    points: [880, 248, 1200, 505],
     tension: 0.5,
     pointerLength: 10,
     pointerWidth: 10,
@@ -247,7 +248,7 @@ var arrow2 = new Konva.Arrow({
 });
 
 var arrow3 = new Konva.Arrow({
-    points: [880, 348, 1200, 595],
+    points: [880, 348, 1200, 505],
     tension: 0.5,
     pointerLength: 10,
     pointerWidth: 10,
@@ -258,7 +259,7 @@ var arrow3 = new Konva.Arrow({
 });
 
 /* for the arrow */
-var arrowarray2 = [880, 248, 1200, 595];
+var arrowarray2 = [880, 248, 1200, 505];
 
 // move arrow 2 up
 function moveArrowUp(){
@@ -266,7 +267,7 @@ function moveArrowUp(){
     stage.find('#arrow2').points(arrowarray2);
 }
 
-var arrowarray1 = [880, 148, 1200, 395];
+var arrowarray1 = [880, 148, 1200, 305];
 
 // move arrow 1 down
 function moveArrowDown(){
@@ -302,27 +303,25 @@ function makeCode(x,y,str,id) {
         fontSize: 18,
         fontFamily: 'Calibri',
         fill: '#000000',
-        width: 400,
+        width: 550,
         padding: 20,
     });
 }
 
-var code=["1. int * p = new int;",
-	"2. *p = 17;",
-	"3. cout << *p << endl;",
-	"4. int * q;",
-	"5. q = new int;",
-	"6. *q = *p;",
-	"7. *p = 27;",
-	"8. cout << *p << \" \" << *q << endl;",
-	"9. int * temp = q;",
-	"10. q = p;",
-	"11. p = temp;",
-	"12. cout << *p << \" \" << *q << endl;",
-	"13. delete p;",
-	"14. delete q;",
+var code=["0. int main(){",
+	"1. \t\t\t std::cout << \"Enter the size of the array: \";",
+	"2. \t\t\t int n, i;",
+	"3. \t\t\t std::cin >> n;",
+	"4. \t\t\t double *a = new double[n];",
+	"5. \t\t\t for (i=0; i<n; ++i) { a[i] = sqrt[i]; }",
+	"6. \t\t\t for (i=0; i<n; ++i) {",
+	"7. \t\t\t \t\t\t if (double(int(a[i])) == a[i] )",
+	"8. \t\t\t \t\t\t \t\t\t std::cout << i << \" is a perfect square \" << std::endl;",
+	"9. \t\t\t }",
+	"10. \t delete [] a;",
+	"11. \t return 0;",
+	"12. }",
 ];
-
 
 function makeBold(id){
 	for(let i=1; i<=code.length; i++){
@@ -330,6 +329,17 @@ function makeBold(id){
 			stage.find('#line'+i).fontStyle('normal');
 		}else{
 			stage.find('#line'+id).fontStyle('bold');
+		}
+	}
+}
+
+// make multiple lines bold
+function makeBold2(id, len){
+	for(let i=1; i<=code.length; i++){
+		if( i<id || i>=(id+len) ){
+			stage.find('#line'+i).fontStyle('normal');
+		}else{
+			stage.find('#line'+i).fontStyle('bold');
 		}
 	}
 }
@@ -364,6 +374,7 @@ var msg_box = new Konva.Rect({
 var message=["$",
     "",
     "",
+    "",
     ""];
 
 function updateMessage(msg,i) {
@@ -372,6 +383,12 @@ function updateMessage(msg,i) {
 
 // draw that $ sign, which is a command line prompt.
 makeText(60,480,message[0],'msg0');
+
+var line1 = new Konva.Line({
+        points: [750, 150, 950, 150],
+        stroke: '#343434',
+        strokeWidth: 5,
+      });
 
 // add the layer to the stage
 stage.add(layer);
@@ -383,107 +400,158 @@ var pc=1;
 function nextstep() {
 	if(pc == 1){
 		makeBold(pc);
-		makeText(650,120,"int * p",'p');
-		makeText(1120,320,"int",'p_heap');
+		//makeText(1120,320,"int",'p_heap');
 		// changing color to mark it as allocated
-                stage.find('#hrec_2').fill('#ecbeb4');
+                //stage.find('#hrec_2').fill('#ecbeb4');
 		// this has to be after makeMemory so that the memory doesn't overwrite this.
-		makeContent(800,120,"0x020C",'p_value');
-		makeContent(1280,320,"?",'p_heap_value');
-		// add the arrow here.
-                layer.add(arrow1);
+		//makeContent(800,120,"0x020C",'p_value');
+		//makeContent(1280,320,"?",'p_heap_value');
 		layer.draw();
 		pc=pc+1;
 	}else if(pc == 2){
 		makeBold(pc);
-                stage.find('#p_heap_value').text('17');
+		updateMessage("$ Enter the size of the array: ",0);
+                makeText(60,500,message[1],'msg1');
+                makeText(60,520,message[2],'msg2');
+                makeText(60,540,message[3],'msg3');
+                makeText(60,560,message[4],'msg4');
+                //stage.find('#p_heap_value').text('17');
 		layer.draw();
 		pc=pc+1;
 	}else if(pc == 3){
 		makeBold(pc);
-		updateMessage("$ 17",0);
-                makeText(60,500,message[1],'msg1');
-		updateMessage("$ ",1);
-                makeText(60,520,message[2],'msg2');
-                makeText(60,540,message[3],'msg3');
-		// stage.find('#console_text').text('$ 10 15');
+		makeText(650,100,"int n",'n');
+		makeContent(820,100,"?",'n_value');
+		layer.add(line1);
+		makeText(650,150,"int i",'i');
+		makeContent(820,150,"?",'i_value');
 		layer.draw();
 		pc=pc+1;
 	}else if(pc == 4){
 		makeBold(pc);
-		makeText(650,220,"int * q",'q');
-		makeContent(800,220,"\t\t\t\t?",'q_value');
+		updateMessage("$ Enter the size of the array: 4 // we pretend someone inputs 4 here",0);
+		updateMessage("$", 2);
+                stage.find('#n_value').text('4');
+		//makeText(650,220,"int * q",'q');
+		//makeContent(800,220,"\t\t\t\t?",'q_value');
 		layer.draw();
 		pc=pc+1;
 	}else if(pc == 5){
 		makeBold(pc);
-		makeText(1120,520,"int",'q_heap');
+		makeText(650,220,"double *a",'a');
+		makeText(1070,520,"double [4]",'a_heap');
 		// changing color to mark it as allocated
+                stage.find('#hrec_1').fill('#ecbeb4');
+                stage.find('#hrec_2').fill('#ecbeb4');
+                stage.find('#hrec_3').fill('#ecbeb4');
                 stage.find('#hrec_4').fill('#ecbeb4');
                 stage.find('#q_value').text('0x0204');
-		makeContent(1280,520,"?",'q_heap_value');
-		// add the second arrow here.
-                layer.add(arrow2);
+		makeText(1400, 210, "[3]", "haddr2");
+		makeText(1400, 310, "[2]", "haddr3");
+		makeText(1400, 410, "[1]", "haddr4");
+		makeText(1400, 510, "[0]", "haddr5");
+		makeContent(1274,220," ?",'q_heap_value3');
+		makeContent(1274,320," ?",'q_heap_value2');
+		makeContent(1274,420," ?",'q_heap_value1');
+		makeContent(1274,520," ?",'q_heap_value0');
+		makeContent(800,220,"0x0200",'a_value');
+		// add the 1st arrow here.
+                layer.add(arrow1);
 		layer.draw();
 		pc=pc+1;
 	}else if(pc == 6){
 		makeBold(pc);
-                stage.find('#q_heap_value').text('17');
-		// stage.find('#console_text').text('$ 150 15');
+                stage.find('#q_heap_value0').text('0.00');
+                stage.find('#q_heap_value1').text('1.00');
+                stage.find('#q_heap_value2').text('1.44');
+                stage.find('#q_heap_value3').text('1.73');
+                stage.find('#i_value').text('4');
 		layer.draw();
 		pc=pc+1;
 	}else if(pc == 7){
 		makeBold(pc);
-                stage.find('#p_heap_value').text('27');
+                stage.find('#i_value').text('0');
 		layer.draw();
 		pc=pc+1;
 	}else if(pc == 8){
 		makeBold(pc);
-		updateMessage("$ 27 17",1);
-		updateMessage("$ ",2);
 		layer.draw();
 		pc=pc+1;
 	}else if(pc == 9){
 		makeBold(pc);
-		makeText(650,320,"int * temp",'temp');
-		makeContent(800,320,"0x0204",'temp_value');
-		// add the 3rd arrow here.
-                layer.add(arrow3);
+		updateMessage("$ 0 is a perfect square", 2);
+		updateMessage("$", 3);
 		layer.draw();
 		pc=pc+1;
-	}else if(pc == 10){
+	}else if(pc == 10){	// close curly brace
 		makeBold(pc);
-                stage.find('#q_value').text('0x020C');
-		moveArrowUp();
 		layer.draw();
 		pc=pc+1;
-	}else if(pc == 11){
-		makeBold(pc);
-                stage.find('#p_value').text('0x0204');
-		moveArrowDown();
+	}else if(pc == 11){	// open curly brace
+		makeBold(7);
+                stage.find('#i_value').text('1');
 		layer.draw();
 		pc=pc+1;
-	}else if(pc == 12){
-		makeBold(pc);
-		updateMessage("$ 17 27",2);
-		updateMessage("$ ",3);
+	}else if(pc == 12){	// if statement
+		makeBold(8);
 		layer.draw();
 		pc=pc+1;
 	}else if(pc == 13){
-		makeBold(pc);
-		// changing color to mark it as reclaimed
-                stage.find('#hrec_4').fill('#ddd');
-                stage.find('#text_q_heap').text('');
+		makeBold(9);
+		updateMessage("$ 1 is a perfect square", 3);
+		updateMessage("$ ",4);
 		layer.draw();
 		pc=pc+1;
-	}else if(pc == 14){
-		makeBold(pc);
+	}else if(pc == 14){	// close curly brace
+		makeBold(10);
+		layer.draw();
+		pc=pc+1;
+	}else if(pc == 15){	// open curly brace
+		makeBold(7);
+                stage.find('#i_value').text('2');
+		layer.draw();
+		pc=pc+1;
+	}else if(pc == 16){	// if statement
+		makeBold(8);
+		layer.draw();
+		pc=pc+1;
+	}else if(pc == 17){	// close curly brace
+		makeBold(10);	// skip 9 because if isn't true.
+		layer.draw();
+		pc=pc+1;
+	}else if(pc == 18){	// open curly brace
+		makeBold(7);
+                stage.find('#i_value').text('3');
+		layer.draw();
+		pc=pc+1;
+	}else if(pc == 19){	// if statement
+		makeBold(8);
+		layer.draw();
+		pc=pc+1;
+	}else if(pc == 20){	// close curly brace
+		makeBold(10);	// skip 9 because if isn't true.
+		layer.draw();
+		pc=pc+1;
+	}else if(pc == 21){	// open curl brace, but now i is 4, thus get out of the for loop.
+		makeBold(7);
+                stage.find('#i_value').text('4');
+		layer.draw();
+		pc=pc+1;
+	}else if(pc == 22){
+		makeBold(11);
 		// changing color to mark it as reclaimed
+                stage.find('#hrec_1').fill('#ddd');
                 stage.find('#hrec_2').fill('#ddd');
-                stage.find('#text_p_heap').text('');
+                stage.find('#hrec_3').fill('#ddd');
+                stage.find('#hrec_4').fill('#ddd');
+                stage.find('#text_a_heap').text('');
 		layer.draw();
 		pc=pc+1;
-	}else if(pc == 15){
+	}else if(pc == 23){
+		makeBold(12);
+		layer.draw();
+		pc=pc+1;
+	}else if(pc == 24){
 		alert("End of animation! Refresh the page if you want to re-run the animation.");
 		pc=pc+1;
 	}
